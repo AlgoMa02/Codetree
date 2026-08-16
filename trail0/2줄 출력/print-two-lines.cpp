3f252c9672063ba26a2884cbf9dc73210cefcdc6
AlgoMa02 <k0201306774@gmail.com>
2026-08-16 11:18:20 +0900

    [Passed] 2줄 출력 (2ms, 0MB)

--- trail0/2줄 출력/print-two-lines.cpp
+++ trail0/2줄 출력/print-two-lines.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
     // cout << "Hello\nWorld";  다음과 같이 문자열 중간에 \n을 써도 된다.
+    // 아니면 아래와 같이 두 줄에 걸쳐서 써도 된다!
     cout << "Hello" << "\n";
     cout << "World";
     return 0;
