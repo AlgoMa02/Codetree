3a693a1fc8d599421f8d3f99cb631897f02d9021
AlgoMa02 <k0201306774@gmail.com>
2026-08-16 11:23:19 +0900

    [Passed] 문자 변경하기 (1ms, 0MB)

---
+++ trail0/문자 변경하기/change-charater.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a = 'C';   // 변수 a에 C를 넣는다.
+    a = 'T';        // 그 후 다시 a에 T를 넣어 준다.
+    cout << a;      // a를 출력해준다.
+    return 0;
+}
