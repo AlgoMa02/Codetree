7e09a96fa3f9695322ed1aeb08a74f519d1e8b26
AlgoMa02 <k0201306774@gmail.com>
2026-08-20 14:41:23 +0900

    [Passed] 길이 단위 변환하기 (2ms, 0MB)

--- trail1/길이 단위 변환하기/change-length-unit.cpp
+++ trail1/길이 단위 변환하기/change-length-unit.cpp
@@ -3,14 +3,13 @@ using namespace std;
 
 int main() {
     // 변수 선언
-    double ft = 30.48;
-    int mi = 160934;
+    double a = 9.2, b = 1.3;
 
     cout << fixed;
     cout.precision(1);
 
     // 출력
-    cout << "9.2ft" << " = " << ft * 9.2 << "cm\n";
-    cout << "1.3mi" << " = " << mi * 1.3 << "cm";
+    cout << a << "ft = " << a * 30.48 << "cm\n";
+    cout << b << "mi = " << b * 160934 << "cm";
     return 0;
 }
