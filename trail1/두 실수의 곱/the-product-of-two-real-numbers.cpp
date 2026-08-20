f17158bd16ea458a7ea56cd4059627764a5af806
AlgoMa02 <k0201306774@gmail.com>
2026-08-20 14:43:52 +0900

    [Passed] 두 실수의 곱 (1ms, 0MB)

---
+++ trail1/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 변수 선언
+    double a = 5.26, b = 8.27;
+    
+    cout << fixed;
+    cout.precision(3);
+
+    // 출력 
+    cout << a * b;
+    return 0;
+}
