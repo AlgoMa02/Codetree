5e3dc2e0f9daacf3fd232cbb1d0897fe1d94520f
AlgoMa02 <k0201306774@gmail.com>
2026-08-22 14:10:36 +0900

    [Passed] 입력받아 출력 2 (2ms, 0MB)

---
+++ trail1/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 변수 선언 및 입력
+    int a, b;
+    cin >> a;
+    cin.get();
+    cin >> b;
+
+    // 출력
+    cout << a << b;
+    return 0;
+}
