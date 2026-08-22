acc20bf5a16881f6cd55e82cf53ff39ec98e3b68
AlgoMa02 <k0201306774@gmail.com>
2026-08-22 13:38:34 +0900

    [Passed] 입력받은 값과 합 출력 (1ms, 0MB)

---
+++ trail1/입력받은 값과 합 출력/output-of-value-and-sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 변수 선언 및 입력
+    int A, B;
+    cin >> A >> B;
+
+    // 출력
+    cout << A << " " << B << " " << A + B;
+    return 0;
+}
