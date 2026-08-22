37091b8cab2c33c9d379ea0c11d7de6b9ee84f49
AlgoMa02 <k0201306774@gmail.com>
2026-08-22 14:08:12 +0900

    [Passed] 날짜 변경하여 출력 2 (1ms, 0MB)

---
+++ trail1/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 변수 선언 및 입력
+    int y, d, m;
+    cin >> m;
+    cin.get();
+    cin >> d;
+    cin.get();
+    cin >> y;
+
+    // 출력
+    cout << y << '.' << m << '.' << d;
+    return 0;
+}
