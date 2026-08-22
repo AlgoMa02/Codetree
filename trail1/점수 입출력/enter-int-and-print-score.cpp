f5ea24723f2836c73d921fa558f499248dd6a884
AlgoMa02 <k0201306774@gmail.com>
2026-08-22 13:19:34 +0900

    [Passed] 점수 입출력 (2ms, 0MB)

---
+++ trail1/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 변수 선언
+    int N;
+
+    // 입력
+    cin >> N;
+
+    // 출력
+    cout << "Your score is " << N << " point.";
+    return 0;
+}
