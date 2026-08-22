c701f26f8db4f3d4c2793b60f51c1ef350731e2a
AlgoMa02 <k0201306774@gmail.com>
2026-08-22 13:55:12 +0900

    [Passed] 문자열 순서 바꾸기 (2ms, 0MB)

---
+++ trail1/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 변수 선언 및 입력
+    string S, T;
+    cin >> S >> T;
+
+    // 출력
+    cout << T << "\n" << S;
+    return 0;
+}
