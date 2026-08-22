0f0eafd387262fd155476e686daa7d9bfccd9a79
AlgoMa02 <k0201306774@gmail.com>
2026-08-22 13:10:48 +0900

    [Passed] 변수 값 복사하기 (1ms, 0MB)

---
+++ trail1/변수 값 복사하기/copying-variable-values.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 변수 선언
+    int a = 1, b = 2, c = 3;
+    
+    // 값 변경
+    a = b = c;
+
+    // 출력
+    cout << a << " " << b << " " << c;
+    return 0;
+}
