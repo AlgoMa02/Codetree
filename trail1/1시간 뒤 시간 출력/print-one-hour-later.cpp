969a151e69837cd2f7f67bc69198a607c67def41
AlgoMa02 <k0201306774@gmail.com>
2026-08-22 14:01:00 +0900

    [Passed] 1시간 뒤 시간 출력 (2ms, 0MB)

---
+++ trail1/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 변수 선언 및 입력
+    int h, m;
+    char c;
+    cin >> h >> c >> m;
+
+    // 출력
+    cout << h+1 << c << m;
+    return 0;
+}
