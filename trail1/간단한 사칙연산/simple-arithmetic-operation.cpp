4bc9413cef8468ccede74dd73f5dbf362669cb69
AlgoMa02 <k0201306774@gmail.com>
2026-08-22 18:42:35 +0900

    [Passed] 간단한 사칙연산 (2ms, 0MB)

---
+++ trail1/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 변수 선언 및 입력
+    int A, B;
+    cin >> A >> B;
+
+    // 출력
+    cout << A + B << "\n";
+    cout << A - B << "\n";
+    cout << A / B << "\n";
+    cout << A % B << "\n";
+    return 0;
+}
