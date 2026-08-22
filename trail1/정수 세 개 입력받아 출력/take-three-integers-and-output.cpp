eccf37c4bafea1d402daf247ed307227e8c0b6e8
AlgoMa02 <k0201306774@gmail.com>
2026-08-22 13:45:52 +0900

    [Passed] 정수 세 개 입력받아 출력 (1ms, 0MB)

---
+++ trail1/정수 세 개 입력받아 출력/take-three-integers-and-output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 변수 선언 및 입력
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    // 출력
+    cout << a << " " << b << " " << c;
+    return 0;
+}
