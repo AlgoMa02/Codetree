4891cd325680809f36523978f6bf9bc679d32e8b
AlgoMa02 <k0201306774@gmail.com>
2026-08-25 22:22:10 +0900

    [Passed] 삼항연산자 (1ms, 0MB)

--- trail1/삼항연산자/ternary-operator.cpp
+++ trail1/삼항연산자/ternary-operator.cpp
@@ -6,7 +6,10 @@ int main() {
     int score;
     cin >> score;
 
-    string result = score == 100 ? "pass" : "failure";
-    cout << result;
+    //string result = score == 100 ? "pass" : "failure";
+    //cout << result;
+
+    // 다른 풀이법
+    cout << ((score == 100) ? "pass" : "failure");
     return 0;
 }
